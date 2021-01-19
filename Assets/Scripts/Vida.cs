cc872446943452944cf15fad6d6ce71c73bf9ce6
jimbrecht15 <75838231+jimbrecht15@users.noreply.github.com>
2021-01-19 18:44:29 +0100

    Zombi se mueve

---
+++ Assets/Scripts/Vida.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vida : MonoBehaviour { 
+
+    public float valor = 100;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void recibirDano(float dano)
+    {
+        valor -= dano;
+        if (valor <0)
+        {
+            valor = 0;
+          
+        }
+    }
+}
